74b94b88fd918c52426f513a6fdf2201787c2ac5
Dmytro <sobkod23@gmail.com>
2026-01-22 19:06:54 -0400

    Added AthleteState model
    Added AthleteState model

---
+++ Assets/Scripts/Athletes/Models/AthleteState.cs
@@ -0,0 +1,11 @@
+namespace FormForge.Athletes.Models
+{
+    public class AthleteState
+    {
+        public float Strength { get; set; }
+        public float Endurance { get; set; }
+        public float Mobility { get; set; }
+        public float Fatigue { get; set; }
+        public int CurrentWeek { get; set; }
+    }
+}
