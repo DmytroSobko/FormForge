3becaf80c86d76d0f745a96c1a9005984373f10b
Dmytro <sobkod23@gmail.com>
2026-01-22 19:02:10 -0400

    Added TrainingDay and TrainingPlan models
    Added TrainingDay and TrainingPlan models

---
+++ Assets/Scripts/TrainingPlans/Models/TrainingDay.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using FormForge.Data.Exercises;
+
+namespace FormForge.TrainingPlans.Models
+{
+    public class TrainingDay
+    {
+        public List<ExerciseDefinitionSO> Exercises;
+    }
+}
